7ddb7c67df88c4263bdd38bf6ba88d83f6d01b98
Aditya Lakshminarayanan <aditya.net09@gmail.com>
2020-11-02 22:55:48 -0800

    main gallery with full screen image functionality

---
+++ app/src/main/java/com/bluebird/atmintis/FullScreenImage.java
@@ -0,0 +1,29 @@
+package com.bluebird.atmintis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class FullScreenImage extends AppCompatActivity {
+
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_screen_image);
+
+        imageView = (ImageView) findViewById(R.id.image_view);
+
+        getSupportActionBar().hide();
+
+        Intent intent = getIntent();
+
+        int position = intent.getExtras().getInt("id");
+
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+
+        imageView.setImageResource(imageAdapter.imageArray[position]);
+    }
+}
